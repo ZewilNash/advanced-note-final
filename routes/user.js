6cfb018de0b3d377877c10308927a4f1c4c2595a
ZewilNash <47469009+ZewilNash@users.noreply.github.com>
2023-02-26 06:47:15 +0200

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -3,22 +3,11 @@ const {createAccount , login , forgetPass , resetPass , randomQuote} = require("
 const passport = require("passport")
 const router = express.Router()
 
-const authMiddleWare = (req , res , next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, 
-    Accept, x-client-key, x-client-token, x-client-secret, Authorization");
-     next();
-}
 
-router.post("/signup" , authMiddleWare , createAccount)
-router.post("/login" , authMiddleWare , login)
-router.post("/forgetpass" , authMiddleWare , forgetPass)
-router.put("/resetpass" , authMiddleWare , resetPass)
-// router.get("/signup-google", function(req , res , next){res.header("Access-Control-Allow-Origin", "*"); next()} , passport.authenticate('google' , {scope:['profile']}))
-// router.get("/google/callback" , passport.authenticate('google' , {failureRedirect:"/"}) , (req , res) => {
-//     res.redirect("http://localhost:5000/note.html")
-// })
+router.post("/signup"  , createAccount)
+router.post("/login"  , login)
+router.post("/forgetpass"  , forgetPass)
+router.put("/resetpass"  , resetPass)
 
 router.get("/randomQuote" , randomQuote)
 
